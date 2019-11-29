01b60ca6fbbd1f69b1eeaba5e4270f93ab054fe3
Leandro Naidhig <48226434+Leandro-Naidhig@users.noreply.github.com>
2019-11-29 03:10:33 -0300

    Verificação da posição da imagem

--- main.m
+++ main.m
@@ -18,11 +18,42 @@ clear all, close all, clc;
 pkg load image;
 
 % Funcao responsavel por reconhecer a cedula escolhida
-function reconheceCedula(nota)
+function reconheceCedula(nota, op)
   
-  figure, imshow(nota);
+  % Transformacao da imagem RGB em escala de cinza
   notaGray = imresize(rgb2gray(nota), [199 447]); 
+  
+  % Binariza a imagem e verifica sua respectiva area
+  notaGraybw = im2bw(notaGray);
+  area = bwarea(~notaGray);
+  
+  % Verifica se a imagem esta dentros dos padroes se sua area
+  % Caso nao estiver, ela esta invertidade e a rotacao e necessaria
+  if(area >= 8100 && area <= 8500 && op == 1) 
+    notaGraybw = imrotate(notaGraybw,   180);
+    
+  elseif(area >= 5600 && area <= 5900 && op == 2) 
+    notaGraybw = imrotate(notaGraybw, 180);
+
+  elseif(area >= 8000 && area <= 8300 && op == 3) 
+    notaGraybw = imrotate(notaGraybw, 180);
+
+  elseif(area >= 2800 && area <= 2900 && op == 4)  
+    notaGraybw = imrotate(notaGraybw, 180); 
+
+  elseif(area >= 2200 && area <= 2300 && op == 5) 
+    notaGraybw = imrotate(notaGraybw, 180);
+
+  elseif(area >= 11000 && area <= 12000 && op == 6) 
+    notaGraybw = imrotate(notaGraybw, 180);
+  
+  end
+  
+  % Visualizacao das notas depois do tratamento
+  figure, imshow(nota);
   figure, imshow(notaGray);
+  
+  % Captura do tamanho da imagem e da indicacao da localizacao da mesma  
   [lin col] = size(notaGray);
   frame2 = notaGray;
   x_ini = col - 100;
@@ -44,8 +75,6 @@ function reconheceCedula(nota)
 
 endfunction
 
-
-
 % Executa o procedimento até o usuário sair do programa
 while(1)
 
@@ -53,36 +82,43 @@ while(1)
   op = menu("Escolha uma opção:", "1) 2 reais ","2) 5 reais",
             "3) 10 reais", "4) 20 reais", "5) 50 reais", "6) 100 reais", "7) Sair");
 
+  % Caso a escolha seja a nota de 2 reais
   if(op == 1)
     nota2 = imread('2.PNG');
-    reconheceCedula(nota2);
+    reconheceCedula(nota2, op);
   endif
 
+  % Caso a escolha seja a nota de 5 reais
   if(op == 2)
     nota5 = imread('5.PNG');
-    reconheceCedula(nota5);
+    reconheceCedula(nota5, op);
   endif
 
+  % Caso a escolha seja a nota de 10 reais
   if(op == 3)
     nota10  = imread('10.PNG');
-    reconheceCedula(nota10);
+    reconheceCedula(nota10, op);
   endif
 
+  % Caso a escolha seja a nota de 20 reais
   if(op == 4)
     nota20  = imread('20.PNG');
-    reconheceCedula(nota20);
+    reconheceCedula(nota20, op);
   endif
 
+  % Caso a escolha seja a nota de 50 reais
   if(op == 5)
     nota50 = imread('50.PNG');
-    reconheceCedula(nota50);
+    reconheceCedula(nota50, op);
   endif
 
+  % Caso a escolha seja a nota de 100 reais
   if(op == 6)
     nota100 = imread('100.jpg');
-    reconheceCedula(nota100);
+    reconheceCedula(nota100, op);
   endif
 
+  % Caso o usuario deseja sair da execucao do programa
   if(op == 7)
     break;
   endif
